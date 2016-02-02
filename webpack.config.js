b511d55335ba5e3d7e6bec674d88ef671aac052c
ling.dingl <ling.dingl@alibaba-inc.com>
2016-02-02 15:36:06 +0800

    refactor(lodash): refactor the import path of lodash function

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,32 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/lib',
-    filename: 'index.js',
+    filename: 'ReactSmooth.js',
+    library: 'ReactSmooth',
     libraryTarget: 'umd',
   },
+  externals: [{
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  }, {
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'ReactDOM',
+      commonjs: 'ReactDOM',
+      amd: 'ReactDOM'
+    }
+  }, {
+    'react-addons-transition-group': {
+      root: 'ReactTransitionGroup',
+      commonjs2: 'ReactTransitionGroup',
+      commonjs: 'ReactTransitionGroup',
+      amd: 'ReactTransitionGroup'
+    }
+  }],
   module: {
     loaders: [
       {
