d807aa9e05ab047aad2f7b98af5895a24a5a23d3
jasonHzq <hzongquan@126.com>
2016-03-08 16:39:15 +0800

    chore: test browser runtime change from phantom to chrome

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
     preprocessors: {
       'test/spec.js': ['webpack', 'sourcemap'],
     },
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     webpack: {
       devtool: 'inline-source-map',
       module: {
