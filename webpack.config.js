2fac297c2a59e56832b265eb1605a256ff9dc9e4
arcthur <arthurtemptation@gmail.com>
2016-03-16 11:43:07 +0800

    chore: use webpack2 and update pkg

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
-var webpack = require('webpack')
+var webpack = require('webpack');
+var env = process.env.NODE_ENV
 
 module.exports = {
   entry: './src/index.js',
@@ -10,47 +11,64 @@ module.exports = {
     libraryTarget: 'umd',
   },
 
-  externals: [{
-    'react': {
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./src'),
+      'node_modules'
+    ],
+    mainFields: ['jsnext:main', 'main'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'react-addons-transition-group': path.resolve(__dirname, './node_modules/react-addons-transition-group'),
+    }
+  },
+
+  externals: {
+    "react": {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react'
-    }
-  }, {
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'ReactDOM',
-      commonjs: 'ReactDOM',
-      amd: 'ReactDOM'
-    }
-  }, {
-    'react-addons-transition-group': {
-      root: 'ReactTransitionGroup',
-      commonjs2: 'ReactTransitionGroup',
-      commonjs: 'ReactTransitionGroup',
-      amd: 'ReactTransitionGroup'
-    }
-  }],
-
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-        ],
-        exclude: /node_modules/,
-        loader: 'babel',
-      },
-    ],
+    },
+    'react-dom': 'ReactDOM',
+    'react-addons-transition-group': 'ReactTransitionGroup',
   },
 
   plugins: [
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
+  ],
 };
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  );
+}
