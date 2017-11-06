17eb827180d4d7eff06c47fb6cb4b97bb5599689
ling.dingl <ling.dingl@alibaba-inc.com>
2017-11-06 16:30:45 +0800

    chore: update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -45,5 +45,27 @@ module.exports = function (config) {
       'text-encoding': 'window',
       'react-addons-test-utils': 'react-dom'
     },
+
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      stats: 'errors-only',
+    },
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+    browserNoActivityTimeout: 60000,
   });
 };
