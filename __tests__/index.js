04f0cc33015b5798560d29c709327a0d854c7ba0
jmfrancois <jmfrancois@talend.com>
2020-12-21 16:26:02 +0100

    chore: move test outside

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Animate, { translateStyle } from './index';
+import Animate, { translateStyle } from '../src/index';
 
 
 describe('Animate', () => {
