49a343afcdf0d84a0f2d76999a6a208db06de026
jmfrancois <jmfrancois@talend.com>
2022-05-23 09:28:25 +0200

    chore: fix static demo option

--- setupTests.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { configure } from 'enzyme';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
